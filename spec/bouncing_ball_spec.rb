e45d7a4adff0c08b78406e24659feee7b71acec8
Shendidy <sherif@shendidy.com>
2019-05-07 15:59:18 +0100

    added new test with different result expected

--- spec/bouncing_ball_spec.rb
+++ spec/bouncing_ball_spec.rb
@@ -4,4 +4,9 @@ describe Bouncing_Ball do
   it '1- should return 3' do
     expect(subject.bouncing_ball(3, 0.66, 1.5)).to eq(3)
   end
+
+  it '2- should return 15' do
+    expect(subject.bouncing_ball(30, 0.66, 1.5)).to eq(15)
+  end
+
 end
