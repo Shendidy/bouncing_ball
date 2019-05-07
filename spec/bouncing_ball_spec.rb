52698b13abc5b767a289b51c981a095db677b5d2
Shendidy <sherif@shendidy.com>
2019-05-07 16:41:20 +0100

    passed all acceptance criteria

--- spec/bouncing_ball_spec.rb
+++ spec/bouncing_ball_spec.rb
@@ -17,4 +17,16 @@ describe Bouncing_Ball do
     expect(subject.bouncing_ball(30, 0.75, 1.5)).to eq(21)
   end
 
+  it '5- should return 3' do
+    expect(subject.bouncing_ball(30, 0.4, 10)).to eq(3)
+  end
+
+  it '6- should return -1' do
+    expect(subject.bouncing_ball(40, 1, 10)).to eq(-1)
+  end
+
+  it '7- should return -1' do
+    expect(subject.bouncing_ball(-5, 0.66, 1.5)).to eq(-1)
+  end
+
 end
