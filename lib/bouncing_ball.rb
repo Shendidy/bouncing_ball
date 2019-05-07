688d59dcf10316fd41aaf52a3c3c7037c4a172ec
Shendidy <sherif@shendidy.com>
2019-05-07 16:38:26 +0100

    function returns correct number of times

--- lib/bouncing_ball.rb
+++ lib/bouncing_ball.rb
@@ -1,11 +1,17 @@
 class Bouncing_Ball
 
   def bouncing_ball(h, bounce, window)
+    h = h
     if (h <= 0) || (bounce <= 0) || (bounce >= 1) || (window >= h)
       return -1
     end
-    3
-    # counter = 1 if h > window
+    counter = 0
+    while h >= window
+      counter += 1
+      h *= bounce
+      counter += 1 if h >= window
+    end
+    counter
   end
 
 end
--- spec/bouncing_ball_spec.rb
+++ spec/bouncing_ball_spec.rb
@@ -9,4 +9,8 @@ describe Bouncing_Ball do
     expect(subject.bouncing_ball(0, 0.66, 1.5)).to eq(-1)
   end
 
+  it '3- should return 15' do
+    expect(subject.bouncing_ball(30, 0.66, 1.5)).to eq(15)
+  end
+
 end
